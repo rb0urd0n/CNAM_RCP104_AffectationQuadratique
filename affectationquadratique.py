c1129b93a18d8609bbc11d119d5659366fe0a315
Romain Bourdon <romain.bourdon@digitalairways.com>
2016-05-30 11:33:35 +0200

    First recuitSimule implementation & first tabou

--- affectationquadratique.py
+++ affectationquadratique.py
@@ -80,6 +80,32 @@ def nombreTotaleDeLiaisonsPourModule(m1):
         liaisons += nombresDeLiaisons[m1-1][i]
     return liaisons
 
+def inverseDeuxModulesAleatoirement(dispositionInitiale):
+    module1 = floor(uniform(0, len(dispositionInitiale)))
+    module2 = floor(uniform(0, len(dispositionInitiale)))
+    while module2 == module1:
+        module2 = floor(uniform(0, len(dispositionInitiale)))
+    return inverseDeuxModules(dispositionInitiale, module1, module2)
+
+def inverseDeuxModules(dispositionInitiale, module1, module2):
+    oldValueModule1 = dispositionInitiale[module1]
+    dispositionInitiale[module1] = dispositionInitiale[module2]
+    dispositionInitiale[module2] = oldValueModule1
+    return dispositionInitiale
+
+def renverseSousListe( liste , depart , arrivee ):
+    longueur = len( liste )
+    while( depart!=arrivee):
+        position_depart = liste[depart]
+        liste[depart] = liste[arrivee]
+        liste[arrivee] = position_depart
+        delta = abs( depart - arrivee ) 
+        if (  delta == 1 or delta == (longueur-1)):
+            break
+        depart = (depart+1)%longueur
+        arrivee = (arrivee-1)%longueur
+    return liste
+
 def rechercheExhaustiveAvecElagage():
     meilleureSolutionInitiale = (list(modules), coutSolution(modules))
     solutionDepart = []
@@ -143,14 +169,78 @@ def rechercheExhaustiveAvecElagageAvance():
             listModulesPlaces.pop()
         return meilleure_solution
     return parcours_recursif(cout, meilleureSolutionInitiale , solutionDepart , modulesAPlacer )
-start_time = time()
-laSolution = rechercheExhaustiveAvecElagageAvance()
-elapsed_time = time() - start_time
-nb = 0
-for i in range(nbModules):
-    nb += nombreTotaleDeLiaisonsPourModule(i+1)
+
+def recuitSimule( nbIterations, coeffTemperature ):
+    meilleureDisposition = list(modules)
+    meilleurCout = coutSolution(meilleureDisposition)
+    print( "Solution initiale : ", meilleureDisposition, " - ", meilleurCout)
+
+    dispositionCourante = list( meilleureDisposition )
+    coutCourant = meilleurCout
+    temp = meilleurCout * nbModules 
+
+    temperature = temp
+    
+    for i in range( nbIterations ):
+        nouvelleDisposition = list( dispositionCourante )
+        nouvelleDisposition = inverseDeuxModulesAleatoirement(nouvelleDisposition)
+        nouvelle_longueur = coutSolution( nouvelleDisposition )
+        delta = nouvelle_longueur - coutCourant
+
+        if ( delta < 0) or ( uniform( 0.0, 1.0 ) < exp( -delta/temperature ) ):
+            dispositionCourante = nouvelleDisposition
+            coutCourant = nouvelle_longueur
+            if (coutCourant < meilleurCout):
+                print("Longueur : ", coutCourant, " - Iteration : ", i)
+                meilleureDisposition = list(dispositionCourante)
+                meilleurCout = coutCourant
+                
+        temperature = coeffTemperature * temperature 
+    return ( meilleureDisposition , meilleurCout )
+    
+def algorithmeTabou( solutionInitiale, tailleListeTabou = 10, nbIteration = 500 ):
+    meilleureDisposition = list(solutionInitiale)
+    meilleurCout = coutSolution( meilleureDisposition )
+    meilleureSolution = ( meilleureDisposition , meilleurCout )
+    dispositionCourante = list(meilleureDisposition)
+    coutCourant = meilleurCout
+
+    listeTabou = []
+    for iter in range( nbIteration ):
+        solutionTmp = None
+        coutTmp = None
+        meilleurMouvement = None
+        meilleureSolutionTmp = (None, None)
         
-print("Nombre total de liaisons : ", nb)
+        for i in range( nbModules-1 ):
+            for j in range( i+1 , nbModules ):
+                mouvement = ( i , j )
+                if mouvement not in listeTabou:
+                    solutionTmp = inverseDeuxModules(dispositionCourante, i, j)
+                    coutTmp = coutSolution(solutionTmp)
+                    if meilleureSolutionTmp[0] == None or coutTmp < meilleureSolutionTmp[1]:
+                        meilleureSolutionTmp = (solutionTmp, coutTmp)
+                        meilleurMouvement = mouvement
+        dispositionCourante = list(meilleureSolutionTmp[0])
+        coutCourant = meilleureSolutionTmp[1]
+        if ( coutCourant < meilleurCout ):
+            print("Meilleure solution : ", dispositionCourante, " - ", coutCourant)
+            meilleurCout = coutCourant
+            meilleureSolution = ( list( dispositionCourante ) , meilleurCout )
+        listeTabou.append( meilleurMouvement )
+        if len( listeTabou ) > tailleListeTabou:
+            listeTabou.pop(0)
+    return meilleureSolution
+
+start_time = time()
+#laSolution = rechercheExhaustiveAvecElagageAvance()
+#laSolution = recuitSimule(10000,0.999)
+modules = list(recuitSimule(10000, 0.999)[0])
+laSolution = algorithmeTabou(modules, 20, 10000)
+elapsed_time = time() - start_time
+liste = [11, 1, 5, 8, 12, 10, 2, 7, 9, 3, 4, 6]
+liste2=[6,4,3,9,7,2,10,1,8,5,12,11]
+
 print("elapsed time : ", elapsed_time)
 print("solution : ", laSolution[0])
 print("cout : ", laSolution[1])
